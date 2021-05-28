3ac88ae7572eef1e2fde2954817a74e0fee960d1
iamnotrodger <rodger.retanal24@gmail.com>
2021-05-28 15:34:30 -0400

    [update] Implemented Yup validation on ProjectName

--- src/component/ProjectName/ProjectName.js
+++ src/component/ProjectName/ProjectName.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { updateProjectName } from '../../api/ProjectAPI';
 import useToast, { TOAST_ACTIONS } from '../../context/ToastContext';
 import { createErrorToast } from '../../utils/toast';
-import { validateProjectName } from '../../utils/validate';
+import { ProjectNameSchema } from '../../utils/validate';
 import InputEditable from '../InputEditable';
 
 const ProjectName = ({ name, projectID, onSave, children, ...props }) => {
@@ -10,9 +10,7 @@ const ProjectName = ({ name, projectID, onSave, children, ...props }) => {
 
     const handleSave = async (name) => {
         try {
-            const error = validateProjectName(name);
-            if (error) throw new Error(error);
-
+            await ProjectNameSchema.validate(name);
             await updateProjectName(projectID, name);
             onSave(name);
         } catch (error) {
